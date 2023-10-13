e781e89e124917dcd6d4f7602223cc046edce631
NYAMBAYAR DEMBERELSUREN <d.nyambayar@mn.data-artist.com>
2023-10-13 14:28:30 +0800

    update

--- test_cypress/cypress.config.js
+++ test_cypress/cypress.config.js
@@ -1,9 +1,20 @@
 const { defineConfig } = require("cypress");
 
+const envUrls = {
+  'dev': 'https://beta.multi-chat.data-artist.info/',
+  'stg': 'https://test.multi-chat.data-artist.info/',
+  'prod': 'https://mugen-ai-chat.jp/'
+};
+
+const baseUrl = envUrls[process.env.ENVIRONMENT || 'dev'];
+
 module.exports = defineConfig({
   defaultCommandTimeout: 20000,
   numTestsKeptInMemory: 4,
   experimentalMemoryManagement: true,
+  env: {
+    host: baseUrl,
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
